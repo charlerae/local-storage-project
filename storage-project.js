fd5d1439cbb10e589e901a539f3d7669088b72e5
Aaron <aaronkcarpenter@gmail.com>
2020-04-16 16:02:48 -0500

    Added the storeItem function, which allows us the ability to store the fruit and the quantity selected to the local storage

--- storage-project.js
+++ storage-project.js
@@ -1,15 +1,31 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
+
   // const showCart = () => {
-
+  
   // };
 
-  // const storeItem = () => {
-
-  // };
+  const storeItem = () => {
+    //created a variable that stores the button(by ele id)
+    const button = document.getElementById('add-to-cart');
+    //created an event that listens for clicks on the A2C Button
+    button.addEventListener('click', event => {
+      //Created a variable that stores each item and Grabs the item by ID
+      const items = document.getElementById('items');
+      //Created a varibale that stores each item's qty and Grabs the quantity by ID
+      const quantity = localStorage.getElementById('quantity');
+      //At this point we use the setItem method to Store the items and their respective quantities in Local Storage
+      localStorage.setItem(items, quantity);
+    });
+    showCart();     
+  };
 
   // const removeItem = () => {
 
   // };
 
 });
+
+
+
+
