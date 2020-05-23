e511bfcf5300d9feddd4f3bdc15c72ebcf579ef4
charlerae <contactcharlerae@gmail.com>
2020-05-22 21:01:28 -0400

    Update storage-project.js

--- storage-project.js
+++ storage-project.js
@@ -3,24 +3,31 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
   const showCart = () => {
     const shoppingCart = document.getElementById('shopping-cart');
-    let eats = '';
-    // const eats = items.value;
+    const cartItem = document.createElement('div');
+    shoppingCart.appendChild(cartItem);
+
 
     for(let i = 0; i < localStorage.length; i++) {
       let food = localStorage.key(i);
-      let amount = localStorage.getItem(food);
-      eats = `${amount} ${food}`;
+      let amount = localStorage.getItem(items);
+
+      let eats = amount;
+      // console.log(food);
+      // console.log(amount);
+      // console.log(eats)
+      cartItem.innerHTML = eats;
     }
 
-    return shoppingCart.innerHTML = eats;
+
+
   };
 
   showCart();
 
-=======
+
 
   // const showCart = () => {
-  
+
   // };
 
 
@@ -31,14 +38,19 @@ window.addEventListener("DOMContentLoaded", (event) => {
     button.addEventListener('click', event => {
       //Created a variable that stores each item and Grabs the item by ID
       const items = document.getElementById('items');
+      const testing = items.value;
+      console.log(testing);
       //Created a varibale that stores each item's qty and Grabs the quantity by ID
-      const quantity = localStorage.getElementById('quantity');
+      const quantity = document.getElementById('quantity');
+      console.log(quantity);
       //At this point we use the setItem method to Store the items and their respective quantities in Local Storage
       localStorage.setItem(items, quantity);
     });
-    showCart();     
-  };
 
+    // console.log(items.value);
+    // console.log(quantity.value);
+  };
+  showCart();
   // const removeItem = () => {
 
   // };
@@ -49,6 +61,5 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
 
 
-=======
-// test comment
 
+// test comment
